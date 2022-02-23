2251a16f349ea6775e009846c8371e1b067df35b
ThankGod Davis Ossaija <thankgod.ossaija@asokoinsight.com>
2022-02-23 03:16:50 +0100

    use database connection pooling

--- packages/utils/db.ts
+++ packages/utils/db.ts
@@ -1,15 +1,21 @@
-import { Client } from 'pg';
 import config from 'config';
+import { Pool } from 'pg';
 
 let db;
 
 export function connect() {
-  db = new Client(config.db);
+  db = new Pool(config.db);
   return db.connect();
 }
 
 export async function query(queryString: string, parameters?: any) {
   if (!db) await connect();
-
-  return db.query(queryString, parameters);
+  try {
+    const result = await db.query(queryString, parameters);
+    db.release();
+    return result;
+  } catch (error) {
+    db.release();
+    throw error;
+  }
 }
